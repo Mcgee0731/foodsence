b36b58e7373fab2c5609485048609b6917c3487e
MrMcC <1098508637@qq.com>
2017-08-15 10:39:50 +0800

    1

--- js/food.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('navUl').addEventListener('click',function (e) {
-  console.log(e.target)
-  // if (lis[i].id === 'feature-1') {
-  //   let slides1 = document.getElementById('slides-1')
-  //   slides1.style.zIndex = 100
-  // }
-})
-
